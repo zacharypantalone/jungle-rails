d9edc880b420571e29edc53e718b79fa566bdd84
Zachary Pantalone <zachary.pantalone@gmail.com>
2022-11-04 10:12:46 -0400

    models code cleaned up

--- app/models/current.rb
+++ app/models/current.rb
@@ -1,4 +1,5 @@
 class Current < ActiveSupport::CurrentAttributes
   
   attribute :user
+  
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ApplicationRecord
+
   monetize :price_cents, numericality: true
   mount_uploader :image, ProductImageUploader
 
@@ -8,4 +9,5 @@ class Product < ApplicationRecord
   validates :price, presence: true
   validates :quantity, presence: true
   validates :category, presence: true
+
 end
