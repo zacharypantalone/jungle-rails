b8e6df86c5359e52514a0f0aab0f88a33695332d
Zachary Pantalone <zachary.pantalone@gmail.com>
2022-11-09 16:07:23 -0500

    step 4 complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,19 @@ class User < ApplicationRecord
 
   has_secure_password
 
+  validates :password, presence: true
+  validates :password_confirmation, presence: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :firstname, presence: true 
+  validates :lastname, presence: true
+  validates :password, length: { minimum: 5 } 
+
+  def self.authenticate_with_credentials(email, password)
+    self.find_by(email: email).try(:authenticate, password)
+  end
+
+    
+
+
+
 end
