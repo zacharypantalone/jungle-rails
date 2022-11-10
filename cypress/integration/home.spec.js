05cdd22f9c6978205c0e0dc8c18b3b3448de9fca
Zachary Pantalone <zachary.pantalone@gmail.com>
2022-11-10 12:15:08 -0500

    Feature Spec: Home Page Complete

---
+++ cypress/integration/home.spec.js
@@ -0,0 +1,17 @@
+
+
+describe('go-to homepage', () => {
+  it('visits the websites homepage', () => {
+
+    cy.visit('http://localhost:3000/')
+    
+  });
+
+  it("There is products on the page", () => {
+    cy.get(".products article").should("be.visible");
+  });
+
+  it("There is 12 products on the page", () => {
+    cy.get(".products article").should("have.length", 12);
+  })
+});
